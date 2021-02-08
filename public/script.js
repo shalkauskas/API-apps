17dcb3b9ae3f80db520f91d74f609654e106046c
shalkauskas <igor.shalkauskas@gmail.com>
2021-02-08 11:08:16 -0500

    update

--- public/script.js
+++ public/script.js
@@ -1,23 +1,13 @@
 //jshint esversion:6
 
 $(document).ready(function() {
-  // IP/location API
-function getIP(zip){
-    $.ajax({
-      url: 'https://ipapi.co/json/',
-      data: JSON,
-      success: function(response) {
-        const zip = response.postal;
-      }
-    });
-
-  }
 
   // Weather API
-  let getWeather = (zip) => {
+  let getWeather = (city) => {
+    console.log("Getting weather from: ", city);
     let APIKey = 'ede7670e8ec9da1d4576253840298116';
     $.ajax({
-      url: 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&APPID=' + APIKey,
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + APIKey,
       data: JSON,
       success: function(response) {
         var resultTemp = Math.floor(response.main.temp - 273.15);
@@ -26,7 +16,7 @@ function getIP(zip){
     });
   };
 
-
+////////////////Functions///////////////
   // Default popup for unsued icons
   let popup = () => {
     if ($('img').hasClass('onclick')) {
@@ -35,8 +25,9 @@ function getIP(zip){
       }, 200);
     }
   };
+  function
 
-  // Effect on icon click
+  ////////////////////// Effect on icon click////////////////
   $('img').on('click', function() {
     let iconClass = this.id;
     let icons = ['my-computer', 'bin'];
@@ -60,25 +51,39 @@ function getIP(zip){
       setTimeout(function() {
         $('img').removeClass('onclick');
       }, 200);
-      getIP();
 
 
       // Get weather based on location
     }
     if (iconClass == 'internet-explorer') {
+      // Add clicked icon effect
       $(this).addClass('onclick');
+      // Remove click effect
       setTimeout(function() {
         $('img').removeClass('onclick');
       }, 200);
-
-      // Get weather based on result of IP function?
-getWeather(getIP(zip));
-
-
-
-    } else {
-      console.log('error');
+      // Open window for city prompt
+      $('#myModal').show('fast');
+      // Close if clicked outside modal
+      $(window).click(function(event) {
+        if (event.target.id == "myModal") {
+          $("#myModal").hide('fast');
+        }
+      });
+      // Close button for window
+      $("#close").on('click', (function() {
+        $("#myModal").hide('fast');
+      }));
+      // Get temp for city on city sumbit
+      $('#getTemp').on('click', function() {
+        getWeather($("#city").val());
+        $("#myModal").hide('fast');
+      });
     }
+
+
+
+
   });
 
 
